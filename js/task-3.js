af5a858caf6f06f031853e19177d182fb259b5b4
Deniz <candenizozder08@gmail.com>
2025-02-24 14:07:25 +0300

    Task 3 Done

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,15 @@
 console.log("JS-3 -----------------------------------")
-function getElementWidth(content, padding, border){
-return parseFloat(content) + (parseFloat(padding)+parseFloat(border))*2;
+function checkForSpam(message){
+    const checkMessage = message;
+    if( checkMessage.toLowerCase().includes("spam") || checkMessage.toLowerCase().includes("sale") )
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
 }
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px")); 
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
