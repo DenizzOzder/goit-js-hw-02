3010d79aa7706fdaee9e3394825e625d0cd6887b
Deniz <candenizozder08@gmail.com>
2025-02-26 02:01:42 +0300

    task2(if,missing cod)task3(extra const)task4(extra let) fixed

--- js/task-4.js
+++ js/task-4.js
@@ -1,31 +1,32 @@
 console.log("JS-4 -----------------------------------");
 
-function getShippingCost(country){
-    const area = country;
-    let price;
-    let message;
-    switch (area){
-        case "China":
-            price = 100;
-            console.log(`Shipping to ${area} will cost ${price} credits`);
-            break;
-        case "Chile":
-            price = 250;
-            console.log(`Shipping to ${area} will cost ${price} credits`);
-            break;
-        case "Australia":
-            price = 170;
-            console.log(`Shipping to ${area} will cost ${price} credits`);
-            break;
-        case "Jamaica":
-            price = 120;
-            console.log(`Shipping to ${area} will cost ${price} credits`);
-            break;
-        default:
-            console.log("Sorry, there is no delivery to your country");
-            break;
+function getShippingCost(country)
+    {
+        const area = country;
+        let price;
+        switch (area)
+        {
+            case "China":
+                price = 100;
+                console.log(`Shipping to ${area} will cost ${price} credits`);
+                break;
+            case "Chile":
+                price = 250;
+                console.log(`Shipping to ${area} will cost ${price} credits`);
+                break;
+            case "Australia":
+                price = 170;
+                console.log(`Shipping to ${area} will cost ${price} credits`);
+                break;
+            case "Jamaica":
+                price = 120;
+                console.log(`Shipping to ${area} will cost ${price} credits`);
+                break;
+            default:
+                console.log("Sorry, there is no delivery to your country");
+                break;
+        }
     }
-}
 getShippingCost("Australia"); // "Shipping to Australia will cost 170 credits"
 getShippingCost("Germany"); // "Sorry, there is no delivery to your country"
 getShippingCost("China"); // "Shipping to China will cost 100 credits"
